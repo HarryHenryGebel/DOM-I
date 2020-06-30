afa8364ea22f18a42e4ff2e08edb9eb8ebb7da77
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-30 01:20:52 -0400

    Additional styling

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -5,6 +5,8 @@ const button = document.createElement('button');
 button.setAttribute('id', 'start-button');
 button.textContent = "Start Timer";
 button.disabled = false;
+button.style.width = "7rem";
 
 const body = document.getElementsByTagName('body')[0];
+body.style.flexDirection = "column";
 body.appendChild(button);
