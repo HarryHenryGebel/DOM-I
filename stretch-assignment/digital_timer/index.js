82b4c5994719ca3c9bdd4cea107bb52bdcfc1eea
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-30 02:49:54 -0400

    Add startTimer (untested)

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,5 +1,22 @@
 'use strict';
 
+function startTimer () {
+  function timeProcessor() {
+    // calculate time passed and update display
+    const timePassed = Date.now() - startTime;
+    updateTimeDisplay(timePassed);
+
+    // if 10000 milliseconds have gone by, stop timer
+    if (timePassed >= 10000) {
+      window.clearInterval(interval);
+      button.disabled = false;
+      button.textContent = "Restart Timer";
+      updateTimeDisplay(0); // reset display
+    }
+  }
+  const startTime = Date.now(); // store the time the timer started
+  const interval = window.setInterval(timeProcessor, 10);
+}
 const button = document.createElement('button');
 button.setAttribute('id', 'start-button');
 button.textContent = "Start Timer";
