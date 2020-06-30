ebdeb2f50c39319dc06ab6dad535890f408fb813
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-30 01:22:20 -0400

    Add onclick listener to button

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,6 +6,11 @@ button.setAttribute('id', 'start-button');
 button.textContent = "Start Timer";
 button.disabled = false;
 button.style.width = "7rem";
+button.onclick = function(event) {
+  this.disabled = true;
+  this.textContent = "Running";
+  startTimer();
+};
 
 const body = document.getElementsByTagName('body')[0];
 body.style.flexDirection = "column";
