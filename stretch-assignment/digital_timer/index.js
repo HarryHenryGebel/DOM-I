f8b70d5caebd6e01b041b6419158902ea574197b
Harry Henry Gebel <hhgebel@gmail.com>
2020-06-30 00:58:38 -0400

    Create button, add to document

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,2 +1,10 @@
 'use strict';
 skewer(); // connect to emacs REPL
+
+const button = document.createElement('button');
+button.setAttribute('id', 'start-button');
+button.textContent = "Start Timer";
+button.disabled = false;
+
+const body = document.getElementsByTagName('body')[0];
+body.appendChild(button);
